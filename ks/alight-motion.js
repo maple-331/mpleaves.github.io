15451963534c6a6b17f8d02e53c32b65d827d89a
maple-331 <kmaple69@gmail.com>
2025-09-07 04:50:29 +0000

    Update alight-motion.js

--- ks/alight-motion.js
+++ ks/alight-motion.js
@@ -20,7 +20,7 @@ objc = {
         "type": "subscription",
         "store": "apple_app_store",
         "autoRenewing": true,
-        "orderNumber": ⚠️快手：AM小秋 其余均为假冒 如您在其他地方购买了此配置那么说明您已被骗，您将无法享受售后且有停用风险 认准快手AM小秋⚠️会员成功解锁",
+        "orderNumber": "⚠️快手：AM小秋 其余均为假冒 如您在其他地方购买了此配置那么说明您已被骗，您将无法享受售后且有停用风险 认准快手AM小秋⚠️会员成功解锁",
         "productId": "alightcreative.motion.1y_t60_1w",
         "period": "1y",
         "label": null,
